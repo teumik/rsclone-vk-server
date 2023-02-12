4f4df17a107760faa9b2353f5ec8def0100f01e6
Artem Mokhov <teumik@gmail.com>
2023-02-12 21:02:54 +0400

    feat: add greeting message with link

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,17 @@ import connectDatabase from './database/connectToDatabase';
 import errorMiddleware from './middlewares/error.middleware';
 import loggerMiddleware from './middlewares/logger.middleware';
 import corsMiddleware from './middlewares/cors.middleware';
+import settings from './utils/settings';
 
 dotenv.config();
 const { env } = process;
 const { DB_URL, PORT } = env;
+const { greetingMessage } = settings;
 
 const app = express();
 
 app.listen(PORT || 5555, async () => { await connectDatabase(DB_URL); });
-app.get('/', (req: Request, res: Response) => res.json('RS Clone VK'));
+app.get('/', (req: Request, res: Response) => res.send(greetingMessage));
 app.use(express.json());
 app.use(cookieParser());
 app.use(corsMiddleware);
