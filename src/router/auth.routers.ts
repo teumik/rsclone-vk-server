c01b5d967639fc84373695cd31842472712935bf
Artem Mokhov <teumik@gmail.com>
2023-02-13 11:45:58 +0400

    feat: add activation middleware

--- src/router/auth.routers.ts
+++ src/router/auth.routers.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import authController from '../controllers/auth.controller';
 import authMiddleware from '../middlewares/auth.middleware';
+import activateMiddleware from '../middlewares/activate.middleware';
 
 const router = Router();
 
 router.post('/registration', authController.registration);
-router.post('/login', authController.login);
+router.post('/login', activateMiddleware, authController.login);
 router.get('/logout', authController.logout);
 router.get('/refresh', authController.refresh);
 router.get('/activate/:link', authController.activation);
-router.get('/user', authMiddleware, authController.getUser);
-router.get('/users', authMiddleware, authController.getAllUsers);
+router.get('/user/:id', activateMiddleware, authMiddleware, authController.getUser);
+router.get('/users', activateMiddleware, authMiddleware, authController.getAllUsers);
 
 export default router;
