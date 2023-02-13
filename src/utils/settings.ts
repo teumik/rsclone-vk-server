37830fd6bf100fc111051aafede3051f703e3a21
Artem Mokhov <teumik@gmail.com>
2023-02-13 11:09:14 +0400

    refactor: change settings store

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,7 +1,6 @@
 const settings = {
-  activationDisabled: false,
   corsAllallowed: true,
-  greetingMessage: '<a style="font-size: 72px" href="https://github.com/teumik/rsclone-vk-server" target="_blank">Link to repository and documentation</a>',
+  greetingMessage: '<a href="https://github.com/teumik/rsclone-vk-server" target="_blank">Link to repository and documentation</a>',
 };
 
 Object.preventExtensions(settings);
