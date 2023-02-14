23e499e0ebe209fcc41f9710f5854cb84b43ea48
Artem Mokhov <teumik@gmail.com>
2023-02-15 02:32:03 +0400

    feat: implement info model

---
+++ src/models/info.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+const infoSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, require: true, ref: 'User' },
+  icon: {
+    data: Buffer,
+    contentType: String,
+  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  status: { type: String },
+  familyStatus: { type: String },
+  originCity: { type: String },
+  school: { type: String },
+  university: { type: String },
+  interests: { type: String },
+  lifePosition: { type: String },
+  favoriteMusic: { type: String },
+  favoriteBooks: { type: String },
+  favoriteFilms: { type: String },
+  birthDate: { type: Date },
+});
+
+const Info = model('Info', infoSchema);
+
+export default Info;
