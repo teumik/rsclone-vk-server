2ce534081e8828688a72d51f7f9bd69e8279d8a4
Artem Mokhov <teumik@gmail.com>
2023-02-15 02:34:22 +0400

    refactor: replace with class and add drop db

--- src/database/connectToDatabase.ts
+++ src/database/connectToDatabase.ts
@@ -1,25 +1,43 @@
 import mongoose from 'mongoose';
+import { NextFunction, Request, Response } from 'express';
 import ApiError from '../utils/apiError';
 
-const connectDatabase = async (DB_URL?: string) => {
-  if (!DB_URL) {
-    throw ApiError.serverError({
-      type: 'DBUrlError',
-      message: 'Database URL cannot be empty string',
-    });
-  }
-  try {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(DB_URL);
-  } catch (error) {
-    if (error instanceof Error) {
-      throw ApiError.databaseError({
-        code: 500,
-        type: 'DBConectionError',
-        message: error.message,
+class DatabaseController {
+  connectDatabase = async (DB_URL: string | undefined) => {
+    if (!DB_URL) {
+      throw ApiError.serverError({
+        type: 'DBUrlError',
+        message: 'Database URL cannot be empty string',
       });
     }
-  }
-};
+    try {
+      mongoose.set('strictQuery', false);
+      await mongoose.connect(DB_URL);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw ApiError.databaseError({
+          code: 500,
+          type: 'DBConectionError',
+          message: error.message,
+        });
+      }
+    }
+  };
 
-export default connectDatabase;
+  dropAllCollections = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { db } = mongoose.connection;
+      const collections = await db.listCollections().toArray();
+      collections.map(async (collection) => {
+        await db.dropCollection(collection.name);
+      });
+      res.status(200).json('Clear');
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+const databaseController = new DatabaseController();
+
+export default databaseController;
