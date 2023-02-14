3f0f54a0402c392b24dbedcec8d0adf591327f59
Artem Mokhov <teumik@gmail.com>
2023-02-15 02:32:25 +0400

    feat: implement user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,15 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  roles: [{ type: String, ref: 'Role' }],
+  info: { type: Schema.Types.ObjectId, ref: 'Info' },
+  settings: { type: Schema.Types.ObjectId, ref: 'Settings' },
+  pendingRequest: [{ type: Schema.Types.ObjectId, ref: 'Friends' }],
+  outgoingRequest: [{ type: Schema.Types.ObjectId, ref: 'Friends' }],
+  friends: [{
+    friendDocument: { type: Schema.Types.ObjectId, ref: 'Friends' },
+    friendId: { type: Schema.Types.ObjectId, ref: 'User' },
+  }],
+  isOnline: { type: Boolean, required: true, default: false },
   isActivated: { type: Boolean, default: false },
   activationLink: { type: String },
 });
