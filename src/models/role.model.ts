42a862fa9986acff6ad32c133b75ec041422e18c
Artem Mokhov <teumik@gmail.com>
2023-02-15 02:31:39 +0400

    refactor: delete role model

--- src/models/role.model.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const roleSchema = new Schema({
-  value: { type: String, unique: true, default: 'user' },
-});
-
-const Role = model('Role', roleSchema);
-
-export default Role;
