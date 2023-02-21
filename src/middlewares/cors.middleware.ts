e2afe1067fde634784535bfa7e0a14a93b9b018a
Artem Mokhov <teumik@gmail.com>
2023-02-21 13:57:30 +0400

    refactor: rename var and move into function

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,20 +1,21 @@
+import { env } from 'process';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import ApiError from '../utils/apiError';
 import settings from '../utils/settings';
 
 dotenv.config();
-const { env } = process;
-const WHITELIST = env.WHITELIST?.split(' ');
+const { WHITELIST } = env;
 const { corsAllallowed } = settings;
 
 const corsMiddleware = cors({
   origin: (requestOrigin, callback) => {
-    if (!requestOrigin) return callback(null, true);
     if (!WHITELIST) {
       throw ApiError.serverError({ message: 'Problems with sites whitelist' });
     }
-    if (!WHITELIST.includes(requestOrigin) && !corsAllallowed) {
+    const accessDomainList = WHITELIST.split(' ');
+    if (!requestOrigin) return callback(null, true);
+    if (!accessDomainList.includes(requestOrigin) && !corsAllallowed) {
       const corsError = ApiError.corsError({
         type: 'InvalidURL',
         message: `${requestOrigin} is not allowed`,
