a00eb3af23b65134b00c5be7682a309f8a4b13dc
Artem Mokhov <teumik@gmail.com>
2023-02-26 16:09:59 +0400

    feat: improve search method

--- src/service/search.service.ts
+++ src/service/search.service.ts
@@ -2,67 +2,76 @@ import { env } from 'process';
 import dotenv from 'dotenv';
 import User from '../models/user.model';
 import Info from '../models/info.model';
-import { IUser } from '../utils/authValidate';
+// import { IUser } from '../utils/authValidate';
 import ApiError from '../utils/apiError';
 
 dotenv.config();
 
-interface ISearch {
-  id: string;
-  firstName: string;
-  lastName: string;
-  fullName: string;
-  hometown: string;
-  school: string;
-  university: string;
-  birthDate: string;
-}
+// interface ISearch {
+//   id: string;
+//   firstName: string;
+//   lastName: string;
+//   fullName: string;
+//   hometown: string;
+//   school: string;
+//   university: string;
+//   birthDate: string;
+// }
 
 class SearchService {
-  private searchByInfo = (info: Partial<ISearch>, value: string) => {
-    const checkFields = {
-      id: info.id,
-      firstName: info.firstName,
-      lastName: info.lastName,
-      fullName: info.fullName,
-      hometown: info.hometown,
-      school: info.school,
-      university: info.university,
-      birthDate: info.birthDate,
-    };
-    return Object.values(checkFields).some(((field) => {
-      if (!field) return false;
-      return field.toString().includes(value);
-    }));
-  };
+  // private searchByInfo = (info: Partial<ISearch>, value: RegExp) => {
+  //   const checkFields = {
+  //     id: info.id,
+  //     firstName: info.firstName,
+  //     lastName: info.lastName,
+  //     fullName: info.fullName,
+  //     hometown: info.hometown,
+  //     school: info.school,
+  //     university: info.university,
+  //     birthDate: info.birthDate,
+  //   };
+  //   return Object.values(checkFields).some(((field) => value.test(field || '')));
+  // };
 
-  private searchByUser = (user: Partial<IUser>, value: string) => {
-    const checkFields = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-    };
-    return Object.values(checkFields).some(((field) => {
-      if (!field) return false;
-      return field.toString().includes(value);
-    }));
-  };
+  // private searchByUser = (user: Partial<IUser>, value: RegExp) => {
+  //   const checkFields = {
+  //     id: user.id,
+  //     firstName: user.firstName,
+  //     lastName: user.lastName,
+  //   };
+  //   return Object.values(checkFields).some(((field) => value.test(field || '')));
+  // };
 
   private findUsers = async (value: string) => {
-    const users = await User.find().select('username isOnline info').populate({
-      path: 'info',
-      select: 'avatar fullName',
-    });
-    const searchResult = users.map(async (user) => {
-      if (this.searchByUser(user, value)) return user;
-      if (!user.info) return null;
-      const info = await Info.findOne({ _id: user.info.id });
-      if (!info) return null;
-      if (this.searchByInfo(info, value)) return user;
-      return null;
-    });
-    const resolvedResult = await Promise.all(searchResult);
-    const result = resolvedResult.filter((user) => user);
+    const regex = new RegExp(value, 'i');
+    const users = await User
+      .find({
+        username: regex,
+      })
+      .select('username isOnline info')
+      .populate({
+        path: 'info',
+        select: 'avatar fullName hometown -_id',
+      });
+    const infos = await Info.find({
+      $or: [
+        { fullName: { $regex: regex } },
+        { hometown: { $regex: regex } },
+      ],
+    })
+      .select('user -_id')
+      .populate({
+        path: 'user',
+        select: 'username isOnline info',
+        populate: {
+          path: 'info',
+          select: 'avatar fullName -_id',
+        },
+      });
+    const idx = users.map((el) => el.id);
+    const infosData = infos.map((el) => el.user);
+    const result = [...users, ...infosData.filter((el) => !idx.includes(el?.id))];
+
     return result;
   };
 
